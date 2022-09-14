03d96a241d05f8b3118afeea465326e6d22b8cb8
angusin <angusin@hotmail.com>
2022-09-14 16:35:55 +0200

    Update on form change and favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,6 @@ import { computed } from "mobx";
 import { DataStoreService } from "./services/data.store.service";
 import { SingleTestConfig } from "./types/types";
 
-const INITIAL_CONFIG: SingleTestConfig = {
-  testId: 0,
-  attackDiceType: 10,
-  attackDiceNumber: 0,
-  defenseArmor: 6,
-  aiming: true,
-  inCover: true,
-};
-
 @Component({
   template: ` <div class="page-wrapper with-navbar">
     <!-- Navbar (immediate child of the page wrapper) -->
@@ -23,19 +14,19 @@ const INITIAL_CONFIG: SingleTestConfig = {
         <div class="row">
           <div class="col-sm">
             <app-manual-test
-              [config]="initialConfigTest1"
+              [config]="configTest1"
               [color]="'rgba(248, 204, 88, 1)'"
             ></app-manual-test>
           </div>
           <div class="col-sm">
             <app-manual-test
-              [config]="initialConfigTest2"
+              [config]="configTest2"
               [color]="'rgba(240, 99, 122, 1)'"
             ></app-manual-test>
           </div>
           <div class="col-sm">
             <app-manual-test
-              [config]="initialConfigTest3"
+              [config]="configTest3"
               [color]="'rgba(71, 146, 228, 1)'"
             ></app-manual-test>
           </div>
@@ -49,10 +40,6 @@ const INITIAL_CONFIG: SingleTestConfig = {
 })
 export class AppComponent {
   constructor(private dataStoreService: DataStoreService) {}
-
-  initialConfigTest1 = { ...INITIAL_CONFIG, testId: 1, attackDiceNumber: 2 };
-  initialConfigTest2 = { ...INITIAL_CONFIG, testId: 2, attackDiceNumber: 3 };
-  initialConfigTest3 = { ...INITIAL_CONFIG, testId: 3, attackDiceNumber: 4 };
 
   @computed get configTest1(): SingleTestConfig {
     return this.dataStoreService.configTest1;
--- src/app/services/data.store.service.ts
+++ src/app/services/data.store.service.ts
@@ -3,18 +3,50 @@ import { action, observable } from "mobx";
 import { SingleTestConfig, RollResult } from "../types/types";
 
 const NUMBER_OF_ROLLS: number = 1000;
+const INITIAL_CONFIG: SingleTestConfig = {
+  testId: 0,
+  attackDiceType: 10,
+  attackDiceNumber: 0,
+  defenseArmor: 6,
+  aiming: true,
+  inCover: true,
+};
 
 @Injectable({
   providedIn: "root",
 })
 export class DataStoreService {
-  @observable configTest1: SingleTestConfig;
-  @observable configTest2: SingleTestConfig;
-  @observable configTest3: SingleTestConfig;
+  @observable configTest1: SingleTestConfig = {
+    ...INITIAL_CONFIG,
+    testId: 1,
+    attackDiceNumber: 2,
+  };
+  @observable configTest2: SingleTestConfig = {
+    ...INITIAL_CONFIG,
+    testId: 2,
+    attackDiceNumber: 3,
+  };
+  @observable configTest3: SingleTestConfig = {
+    ...INITIAL_CONFIG,
+    testId: 3,
+    attackDiceNumber: 4,
+  };
 
-  @observable resultTest1: RollResult;
-  @observable resultTest2: RollResult;
-  @observable resultTest3: RollResult;
+  @observable resultTest1: RollResult = {
+    testId: 1,
+    averageWounds: 0,
+    chartData: [],
+  };
+  @observable resultTest2: RollResult = {
+    testId: 1,
+    averageWounds: 0,
+    chartData: [],
+  };
+  @observable resultTest3: RollResult = {
+    testId: 1,
+    averageWounds: 0,
+    chartData: [],
+  };
 
   @action setConfig(config: SingleTestConfig) {
     switch (config.testId) {
@@ -33,6 +65,7 @@ export class DataStoreService {
   }
 
   @action setResult(result: RollResult) {
+    console.log(result);
     switch (result.testId) {
       case 1:
         this.resultTest1 = result;
@@ -50,7 +83,15 @@ export class DataStoreService {
 
   constructor() {}
 
-  calculateSuccessPercentage(config: SingleTestConfig) {}
+  calculateSuccessPercentage(config: SingleTestConfig) {
+    console.log(config);
+    const avgWounds: number = config.attackDiceNumber + +config.attackDiceType;
+    this.setResult({
+      testId: config.testId,
+      averageWounds: avgWounds,
+      chartData: [],
+    });
+  }
 
   calculateAverageWounds(): number {
     return 0;
